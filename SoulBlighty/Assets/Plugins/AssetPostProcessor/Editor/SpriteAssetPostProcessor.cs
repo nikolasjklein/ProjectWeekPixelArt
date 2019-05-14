5383715f6baadbfa4f8a8c7ec62de6915df2f6be
Nikolas Joshua Klein <the@nikolasjklein.com>
2019-05-14 12:59:52 +0200

    Implemented Climb Scene and Pixel Perfection

---
+++ SoulBlighty/Assets/Plugins/AssetPostProcessor/Editor/SpriteAssetPostProcessor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using System.IO;
+
+namespace CraftingLegends.SceneTools
+{
+	public class SpriteAssetPostProcessor : AssetPostprocessor
+	{
+		private const float PIXEL_PER_UNIT = 16f;
+
+		// called the first time a sprite is created etc.
+		// not called when only pixels were changed
+		private void OnPostprocessSprites(Texture2D texture, Sprite[] sprites)
+		{
+			TextureImporter importer = assetImporter as TextureImporter;
+
+			// we only change sprite import settings on first import, so we can change those settings for individual files
+			string name = importer.assetPath.ToLower();
+
+			if (File.Exists(AssetDatabase.GetTextMetaFilePathFromAssetPath(name)))
+			{
+				return;
+			}
+
+			// adjust values for pixel art
+
+			importer.spritePixelsPerUnit = PIXEL_PER_UNIT;
+			importer.mipmapEnabled = false;
+			importer.filterMode = FilterMode.Point;
+			importer.textureCompression = TextureImporterCompression.Uncompressed;
+
+			importer.textureType = TextureImporterType.Sprite;
+			//importer.textureType = TextureImporterType.Advanced;
+			//importer.isReadable = true;
+
+			// access the TextureImporterSettings to change the spriteAlignment value
+
+			TextureImporterSettings textureSettings = new TextureImporterSettings();
+			importer.ReadTextureSettings(textureSettings);
+
+			textureSettings.spritePivot = new Vector2(0.5f, 0f);
+			textureSettings.spriteAlignment = (int)SpriteAlignment.BottomCenter;
+			textureSettings.spriteMeshType = SpriteMeshType.FullRect;
+
+			importer.SetTextureSettings(textureSettings);
+
+			importer.SaveAndReimport();
+		}
+	}
+}
---
+++ SoulBlighty/Assets/Scripts/PlayerClimbController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClimbController : MonoBehaviour
+{
+    private GameObject Player;
+    public GameObject Prompt_Left;
+    public GameObject Prompt_Right;
+    public int ClimbState;
+    public Animator playerAnimator;
+    public bool canClimbLeft;
+    public bool canClimbRight;
+    public bool canReachTop;
+    public Animator Shine;
+
+    public void Start()
+    {
+        Player = this.gameObject;
+        canClimbLeft = false;
+        canClimbRight = true;
+        canReachTop = true;
+        Prompt_Right.gameObject.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if (canClimbRight == true && Input.GetKey(KeyCode.Joystick1Button0))
+        {
+            if (ClimbState < 16)
+            {
+                Prompt_Left.gameObject.SetActive(false);
+                ClimbState += 1;
+                playerAnimator.Play("player_climb_lefttoright");
+                StartCoroutine("PlayLeftClimbAnim");
+                canClimbRight = false;
+                canClimbLeft = true;
+            }
+        }
+
+        if (canClimbLeft == true && Input.GetKey(KeyCode.Joystick1Button2))
+        {
+            if (ClimbState < 16)
+            {
+                Prompt_Right.gameObject.SetActive(false);
+                ClimbState += 1;
+                playerAnimator.Play("player_climb_righttoleft");
+                StartCoroutine("PlayRightClimbAnim");
+                canClimbLeft = false;
+                canClimbRight = true;
+            }
+        }
+
+        //When Player reaches the Top
+        if (ClimbState == 16)
+        {
+            if (canReachTop == true)
+            {
+                canClimbLeft = false;
+                canClimbRight = false;
+                Destroy(Prompt_Left);
+                Destroy(Prompt_Right);
+
+                this.transform.Translate(Vector3.up * Time.smoothDeltaTime * .6625f);
+                canReachTop = false;
+
+                playerAnimator.Play("player_rise");
+                StartCoroutine("PlayShine");
+            }
+        }
+    }
+
+    IEnumerator PlayLeftClimbAnim()
+    {
+        yield return new WaitForSeconds(.5f);
+        this.transform.Translate(Vector3.up * Time.smoothDeltaTime * .375f);
+        yield return new WaitForSeconds(.2f);
+        Prompt_Right.gameObject.SetActive(true);
+    }
+
+    IEnumerator PlayRightClimbAnim()
+    {
+        yield return new WaitForSeconds(.5f);
+        this.transform.Translate(Vector3.up * Time.smoothDeltaTime * .375f);
+        yield return new WaitForSeconds(.2f);
+        Prompt_Left.gameObject.SetActive(true);
+    }
+
+    IEnumerator PlayShine()
+    {
+        yield return new WaitForSeconds(2.6f);
+        Shine.Play("shine_glow");
+    }
+}
